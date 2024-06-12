654b83a6583c7cb7b9c98cc6dd322d4a5dd2563d
Gautam <gautamus1793@gmail.com>
2024-06-11 23:41:52 -0500

    2nd Commit

--- src/main/java/Learning/demo_Spring_Security/Utils/JwtUtils.java
+++ src/main/java/Learning/demo_Spring_Security/Utils/JwtUtils.java
@@ -49,7 +49,7 @@ public class JwtUtils {
                 .setClaims(extraClaim)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 30))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 120))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
 
